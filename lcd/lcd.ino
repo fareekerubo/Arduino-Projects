5420eea83dbaba979e70db56236a8d3b47562ed0
fareekerubo <kerubofarida@gmail.com>
2019-02-15 14:12:49 +0300

    Changes

--- lcd/lcd.ino
+++ lcd/lcd.ino
@@ -10,20 +10,3 @@ LiquidCrystal lcd(4,6,11,12,13,14); // Creates an LC object. Parameters: (rs, en
 void setup() { 
  lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display } 
 }
-void loop() { 
- lcd.print("Arduino"); // Prints "Arduino" on the LCD 
- delay(3000); // 3 seconds delay 
- lcd.setCursor(2,1); // Sets the location at which subsequent text written to the LCD will be displayed 
- lcd.print("LCD Tutorial"); 
- delay(3000); 
- lcd.clear(); // Clears the display 
- lcd.blink(); //Displays the blinking LCD cursor 
- delay(4000); 
- lcd.setCursor(7,1); 
- delay(3000); 
- lcd.noBlink(); // Turns off the blinking LCD cursor 
- lcd.cursor(); // Displays an underscore (line) at the position to which the next character will be written 
- delay(4000); 
- lcd.noCursor(); // Hides the LCD cursor 
- lcd.clear(); // Clears the LCD screen 
-}
